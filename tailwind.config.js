3cc5685ac60ada191e72a34d9a215198028ab270
ces0491 <cesairetobias@gmail.com>
2025-05-16 15:22:54 +0200

    trying to get back to a working version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sc-green": "#006837", // Dark green from logo
+        "sc-gold": "#F7B917",  // Gold/yellow from logo 
+        "sc-black": "#231F20", // Black from logo
+      }
+    },
   },
   plugins: [],
 }
